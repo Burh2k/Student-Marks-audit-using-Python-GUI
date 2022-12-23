8d71b1ce51c12cef54d95036f14863d392492f56
Gano603 <khawar603@gmail.com>
2022-12-23 19:41:46 +0500

    Use test as reference and implement in project

--- Project.py
+++ Project.py
@@ -1,28 +1,23 @@
-from tkinter import *
 root=Tk()
-root.geometry('1200x800')
+root.geometry('640x480')
 root.title("Teacher Portal")
 root.resizable(True,True)
 
+# def inputt()
+
+
 Label(text="TEACHER PORTAL",bg="black",fg="white",font=("arial",30,"bold"),width="300",height="2").pack()
 
-f=Frame(root,bg="pink",width="350",height="700")
-f.place(x=10,y=118)
+f=Frame(root,width="200",height="350",bg="#aaa")
+f.pack()
 
+f1=LabelFrame(f,text="Functions",bg="#aaa")
+f1.pack()
 
-# def printValue():
-#     pname = player_name.get()
-#     Label(root, text=f'{pname}, Registered!', pady=20, bg='#ffbf00').pack()
+Button(f1,text="New..").pack()
+Button(f1,text="Select").pack()
 
-# player_name = Entry(root)
-# player_name.pack(pady=30)
-
-# Button(
-#     root,
-#     text="Register Player", 
-#     padx=10, 
-#     pady=5,
-#     command=printValue
-#     ).pack()
+f2 = Frame(root, bg="lightyellow", width=350, height=7000)
+f2.place(x=400, y=118)
 
 root.mainloop()
