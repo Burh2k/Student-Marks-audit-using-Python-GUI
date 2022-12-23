44824ce94aff243ed1dbbb89a7ea4f03b0362858
Burhan <burhan.ahmed60090@gmail.com>
2022-12-23 15:28:29 +0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -6,19 +6,19 @@ root.title("Gaming zone")
 root.resizable(True,True)
 root.withdraw()
 
-def printValue():
-    pname = player_name.get()
-    Label(root, text=f'{pname}, Registered!', pady=20, bg='#ffbf00').pack()
+# def printValue():
+#     pname = player_name.get()
+#     Label(root, text=f'{pname}, Registered!', pady=20, bg='#ffbf00').pack()
 
-player_name = Entry(root)
-player_name.pack(pady=30)
+# player_name = Entry(root)
+# player_name.pack(pady=30)
 
-Button(
-    root,
-    text="Register Player", 
-    padx=10, 
-    pady=5,
-    command=printValue
-    ).pack()
+# Button(
+#     root,
+#     text="Register Player", 
+#     padx=10, 
+#     pady=5,
+#     command=printValue
+#     ).pack()
 
 root.mainloop()
